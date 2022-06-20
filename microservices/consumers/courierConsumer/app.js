3f23e9953e683c8088d6a6aedcbead24fb8fb7cc
mcolton <mcolton@us.ibm.com>
2022-06-20 13:32:41 -0500

    updated Mongo connections

--- microservices/consumers/courierConsumer/app.js
+++ microservices/consumers/courierConsumer/app.js
@@ -6,20 +6,20 @@ const Order = require('./models/order.js')
 // environment variables for mongodb connection
 const MONGODB_REPLICA_HOSTNAMES = process.env.MONGODB_REPLICA_HOSTNAMES
 // const MONGODB_REPLICA_SET = process.env.MONGODB_REPLICA_SET
-// const MONGODB_DBNAME = process.env.MONGODB_DBNAME
-// const MONGODB_AUTH_DBNAME = process.env.MONGODB_AUTH_DBNAME || MONGODB_DBNAME
-// const MONGODB_CA_PATH = process.env.MONGODB_CA_PATH
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME
+const MONGODB_AUTH_DBNAME = process.env.MONGODB_AUTH_DBNAME || MONGODB_DBNAME
+const MONGODB_CA_PATH = process.env.MONGODB_CA_PATH
 // const MONGODB_USER = process.env.MONGODB_USER
 // const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
 mongoose
     .connect('mongodb://' + MONGODB_REPLICA_HOSTNAMES + '/', {
         // user: MONGODB_USER,
         // pass: MONGODB_PASSWORD,
-        dbName: "example-food-delivery",
+        dbName: MONGODB_DBNAME,
         // replicaSet: MONGODB_REPLICA_SET,
-        // authSource: MONGODB_AUTH_DBNAME,
-        // tls: true,
-        // tlsCAFile: MONGODB_CA_PATH,
+        authSource: MONGODB_AUTH_DBNAME,
+        tls: true,
+        tlsCAFile: MONGODB_CA_PATH,
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true})
